b78deb64455827dd3ec1ef348e03e9d8702c2b92
rodrigomarcolin <rodrigosalesmarcolin@gmail.com>
2025-05-30 19:24:45 -0300

    :sparkles: use posthog

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,6 +5,19 @@ import Router from './Router'
 
 const AppRouter = import.meta.env.VITE_USE_HASH_ROUTE === 'true' ? HashRouter : BrowserRouter
 
+import posthog from 'posthog-js'
+
+const posthogToken = import.meta.env.VITE_POSTHOG_TOKEN
+if (!posthogToken) {
+    console.warn('PostHog token is not set. Analytics will not be initialized.')
+} else {
+    posthog.init(posthogToken,
+        {
+            api_host: 'https://us.i.posthog.com',
+            person_profiles: 'always' 
+        }
+)
+
 export default function App() {
     return (
         <ThemeProvider>
